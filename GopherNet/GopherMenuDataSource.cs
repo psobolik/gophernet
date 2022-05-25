f6663943d1cd56391865f19da6df772c1ba71265
Paul Sobolik <psobolik@zoho.com>
2022-05-25 18:57:52 -0400

    Resharpered

--- GopherNet/GopherMenuDataSource.cs
+++ GopherNet/GopherMenuDataSource.cs
@@ -1,13 +1,9 @@
 ﻿using GopherLib.Models;
 using NStack;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terminal.Gui;
-
 
 namespace GopherNet
 {
@@ -20,7 +16,7 @@ namespace GopherNet
             GopherEntities = gopherMenu.GopherEntities.ToList();
         }
 
-        public int Count => GopherEntities != null ? GopherEntities.Count() : 0;
+        public int Count => GopherEntities?.Count ?? 0;
 
         public int Length => Count;
 
@@ -31,13 +27,13 @@ namespace GopherNet
         }
 
         // This is lifted from Terminal.GUI's ListView implementation
-        void RenderUstr(ConsoleDriver driver, ustring ustr, int col, int line, int width)
+        void RenderUstr(ConsoleDriver driver, ustring ustr, int width)
         {
-            int byteLen = ustr.Length;
-            int used = 0;
-            for (int i = 0; i < byteLen;)
+            var byteLen = ustr.Length;
+            var used = 0;
+            for (var i = 0; i < byteLen;)
             {
-                (var rune, var size) = Utf8.DecodeRune(ustr, i, i - byteLen);
+                var (rune, size) = Utf8.DecodeRune(ustr, i, i - byteLen);
                 var count = System.Rune.ColumnWidth(rune);
                 if (used + count > width)
                     break;
@@ -51,38 +47,46 @@ namespace GopherNet
             }
         }
 
-        public void Render(ListView container, ConsoleDriver driver, bool selected, int item, int col, int line, int width, int start = 0)
+        public void Render(ListView container, ConsoleDriver driver, bool selected, int item, int col, int line,
+            int width, int start = 0)
         {
             container.Move(col, line);
             var gopherEntity = GopherEntities[item];
-            char icon = ' ';
             if (gopherEntity == null)
             {
-                RenderUstr(driver, ustring.Make(""), col, line, width);
+                RenderUstr(driver, ustring.Make(""), width);
             }
-            else if (gopherEntity.IsDirectory)
+            else
             {
-                icon = '\x25ba';
+                char icon;
+                if (gopherEntity.IsDirectory)
+                {
+                    icon = '\x25ba';
+                }
+                else if (gopherEntity.IsDocument)
+                {
+                    icon = '\x25a0';
+                }
+                else if (gopherEntity.IsIndexSearch)
+                {
+                    icon = '\x263c';
+                }
+                else if (gopherEntity.IsBinary || gopherEntity.IsEncodedText)
+                {
+                    icon = '\x2193';
+                }
+                else if (!gopherEntity.IsInfo) // Interface or unknown; Unsupported
+                {
+                    icon = '\x00d7';
+                }
+                else
+                {
+                    icon = ' ';
+                }
+
+                var text = $"{icon} {gopherEntity.DisplayText}";
+                RenderUstr(driver, text, width);
             }
-            else if (gopherEntity.IsDocument)
-            {
-                icon = '\x25a0';
-            }
-            else if (gopherEntity.IsIndexSearch)
-            {
-                icon = '\x263c';
-            }
-            else if (gopherEntity.IsBinary || gopherEntity.IsEncodedText)
-            {
-                icon = '\x2193';
-                //icon = gopherEntity.Type;
-            }
-            else if (!gopherEntity.IsInfo) // Interface or unknown; Unsupported
-            {
-                icon = '\x00d7';
-            }
-            var text = $"{icon} {gopherEntity.DisplayText}";
-            RenderUstr(driver, (ustring)text, col, line, width);
         }
 
         public void SetMark(int item, bool value)
